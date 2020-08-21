5755589fd2bbd449f10cf9d166edd82b745271bb
money <1107959724@qq.com>
2020-08-21 23:09:37 +0800

    [王森钱]plan新增修改计划

--- Runner_Commons/src/main/java/com/runner/commons/dto/PlanInfoDto.java
+++ Runner_Commons/src/main/java/com/runner/commons/dto/PlanInfoDto.java
@@ -1,6 +1,8 @@
 package com.runner.commons.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @program: runner
@@ -9,23 +11,25 @@ import lombok.Data;
  * @create: 2020-08-20 19:13
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlanInfoDto {
+    private long planInfoId;
     private String planName;
     private long planLevel;
     private long fitnessGoals;
     private long choiceRun;
     private long choiceInstrument;
-    private double inputHeight;
-    private double inputWeight;
-    private double weightGoals;
+    private Double inputHeight;
+    private Double inputWeight;
+    private Double weightGoals;
     private long weekExerciseDays;
     private long dayExerciseMinute;
     private long planTime;
     //运动器械
     private String instrumentName;
-    private long planInfoId;
     //增肌目标
-    private long gainMusclePart;
+    private Integer gainMusclePart;
     private String partStatus;
     private String improveStatus;
 }
