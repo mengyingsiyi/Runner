4b1675a2d9de8aebc655fc440caa8bb6f55db456
YuanSuai <303218828@qq.com>
2020-08-24 22:35:00 +0800

    [YS]优化接口

--- Runner_Commons/src/main/java/com/runner/commons/dto/SportsDto/ChecKcoureseDto.java
+++ Runner_Commons/src/main/java/com/runner/commons/dto/SportsDto/ChecKcoureseDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class checkcoureseDto {
+public class ChecKcoureseDto {
     private int id;
     private String title;
     private String introduce;
